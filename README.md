518069bc15575aeb7d0d0927694996ff3f3de573
Gustavo Gondim <gustavospgondim@gmail.com>
2020-12-03 16:10:38 -0300

    first working version

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # mapbox-vue-starter-project
 
+## Highlights
+- Project uses Polyfill.io instead Babel for reduced output size
+- Project uses Mapbox GL npm module + Mapbox GL stylesheet also from its module (instead pointing to Mapbox CDN)
+- Basic view (`Home.vue`) with a Mapbox map being loaded through Vue's lifecycle
+- Mapbox object (`map`) is outside the view model to reduce Vue's observables and listeners
+- A GeoJSON data source is loaded directly with `map.addSource()` - you should have an API or an URL that outputs a `FeatureCollection` with the following model:
+```
+{ 
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [ longitude, latitude ]
+      },
+      properties: {
+        id: 'unique & required'
+      }
+    }
+  ]
+}
+```
+- A simple layer of red circles is loaded for the data source
+- A simple `click` event is triggered whenever the user clicks a marker, then it shows the marker detail
+
 ## Project setup
 ```
 npm install
 ```
+
+
 
 ### Compiles and hot-reloads for development
 ```
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="vh-100 vw-100 d-flex">
+    <div class="bg-light p-3" style="width: 400px;">
+      <dl v-if="marker && marker.id" class="text-left">
+        <dd class="d-inline-block mr-2">id</dd>
+        <dt class="d-inline-block">{{ marker.id }}</dt>
+        <span class="d-block" v-for="prop in properties" :key="prop">
+          <dd class="d-inline-block mr-2">{{ prop }}</dd>
+          <dt class="d-inline-block">{{ marker.properties[prop] }}</dt>
+        </span>
+      </dl>
+    </div>
+    <div id="map" class="w-100 h-100" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+
+let map;
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    marker: {},
+  }),
+
+  mounted() {
+    map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-55, -10],
+      zoom: 4,
+    });
+    map.on('style.load', this.mapLoaded);
+  },
+
+  methods: {
+    async mapLoaded() {
+      map.addSource('points', {
+        type: 'geojson',
+        promoteId: 'id',
+        data: 'http://localhost:7071/api/full',
+      });
+      map.addLayer({
+        id: 'markers',
+        type: 'circle',
+        source: 'points',
+        paint: {
+          'circle-radius': ['interpolate', ['linear'], ['zoom'], 10, 2.5, 15, 10],
+          'circle-color': '#f00',
+        },
+      });
+      map.on('click', 'markers', this.markerClick);
+    },
+
+    markerClick(e) {
+      this.marker = e.features[0];
+    },
+  },
 }
 </script>
